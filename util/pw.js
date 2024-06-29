8b6bc879dbca8da4eb9c8c63ac32a2c9bef83a69
Devil-Dude <90880482+skull-fir@users.noreply.github.com>
2024-06-29 18:05:02 +1000

    Update pw.js

--- util/pw.js
+++ util/pw.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
-const botName = "💎 PRIME MUSIC";
-const Text = "GlaceYT";
+const botName = "Grothm";
+const Text = "Grothm Development";
 const version = "Latest@ v1.0"; 
 const startTime = Date.now();
 
